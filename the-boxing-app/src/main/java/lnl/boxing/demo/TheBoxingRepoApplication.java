905acaf79dc1929b492899142b80bf224e4f20b4
JaseP88 <jtpbyd@mail.umsl.edu>
2019-01-25 20:54:17 -0600

    init

---
+++ the-boxing-app/src/main/java/lnl/boxing/demo/TheBoxingRepoApplication.java
@@ -0,0 +1,14 @@
+package lnl.boxing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBoxingRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBoxingRepoApplication.class, args);
+	}
+
+}
+
